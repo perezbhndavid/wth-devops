dee207d537e919dff6ba89fb067a481a8aa1c4c1
perezbhndavid <48489299+perezbhndavid@users.noreply.github.com>
2024-08-27 21:46:23 +0000

    Crear workflow de deploy en azure

--- src/components/Hours.js
+++ src/components/Hours.js
@@ -1,1 +1,12 @@
 // Placeholder to host a component
+import React from 'react';
+
+const HorarioAtencion = () => {
+  return (
+    <div>
+      <p>10 a.m. a 4 p.m. de lunes a viernes, y de 9 a.m. a 8 p.m. los sábados y domingos</p>
+    </div>
+  );
+};
+
+export default HorarioAtencion;
